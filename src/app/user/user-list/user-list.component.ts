14a88fe6419f5fd21723a0d7e44016b35e1f1b7e
xxxdendacion <icesfrost@gmail.com>
2020-08-05 17:01:06 +0300

    add deleteUser function

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserAddComponent } from '../user-add/user-add.component';
 import {User} from '../../models/user';
 import {Role} from '../../models/role';
+import { MatTable } from '@angular/material/table';
 
 
 
 const ELEMENT_DATA: User[] = [
   {firstName: "Den", lastName: 'Wes', email: "test@test.ru", role: 'Art Manager', },
-  {firstName: "Den", lastName: 'Wes', email: "test@test.ru", role: 'Art Manager',},
-  {firstName: "Den", lastName: 'Wes', email: "test@test.ru", role: 'Art Manager', },
-  {firstName: "Den", lastName: 'Wes', email: "test@test.ru", role: 'Art Manager',},
+  {firstName: "Ivan", lastName: 'Wes', email: "test@test.ru", role: 'Art Manager',},
+  {firstName: "Roma", lastName: 'Wes', email: "test@test.ru", role: 'Art Manager', },
+  {firstName: "Alex", lastName: 'Wes', email: "test@test.ru", role: 'Art Manager',},
 ];
 
 @Component({
@@ -20,6 +21,7 @@ const ELEMENT_DATA: User[] = [
 })
 
 export class UserListComponent implements OnInit {
+   @ViewChild(MatTable) table: MatTable<any>;
    roles: Role[] = [
   
     {value:"artist",viewValue: 'Artist'},
@@ -47,9 +49,19 @@ export class UserListComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if(result) {
       this.user = result;
-      console.log(this.user);
+      ELEMENT_DATA.push(this.user);
+      this.dataSource = ELEMENT_DATA;
+      this.table.renderRows();
       }
     });
+  }
+
+  deleteUser(user: User) {
+    if(confirm("Вы хотите удалить пользователя?")) {
+      this.dataSource = this.dataSource.filter(item => {
+        return item != user;
+      });
+    }
   }
 
 }
